ff98cb87a2c9c42f442e74a142087784bf62c9de
liangth <liangth@umich.edu>
2022-11-17 00:42:55 -0500

    add js file to control the video

---
+++ js/video.js
@@ -0,0 +1,71 @@
+// Add js here.
+var video = document.getElementById('videoplayer')
+var play = document.getElementById('play')
+var pause = document.getElementById('pause')
+var slower = document.getElementById('slower')
+var faster = document.getElementById('faster')
+var skip = document.getElementById('skip')
+var mute = document.getElementById('mute')
+var vol = document.getElementById('slider')
+var volumeDisp = document.getElementById('volume')
+
+video.load()
+video.autoplay=false
+video.loop=false
+play.addEventListener(`click`, playVideo)
+pause.addEventListener(`click`, pauseVideo)
+slower.addEventListener(`click`, slowerVideo)
+faster.addEventListener(`click`, fasterVideo)
+skip.addEventListener(`click`, skipVideo)
+mute.addEventListener(`click`, muteVideo)
+vol.addEventListener('input', volVideo)
+
+
+var playRateList = [0.5,1,2]
+var playRateStat = 1
+volumeDisp.innerHTML = vol.value+'%'
+
+function playVideo(){
+    video.play()
+}
+function pauseVideo(){
+    video.pause()
+}
+function slowerVideo(){
+    if (playRateStat==0) {
+        alert("Video is at slowest speed!")
+    }
+    else {
+        playRateStat-=1
+        video.playbackRate = playRateList[playRateStat]
+    }
+}
+function fasterVideo(){
+    if (playRateStat==2) {
+        alert("Video is at fastest speed!")
+    }
+    else {
+        playRateStat+=1
+        video.playbackRate = playRateList[playRateStat]
+    }
+}
+function skipVideo(){
+    video.currentTime += 15
+    if (video.currentTime >=video.duration) {
+        video.currentTime = 0
+    }
+}
+function muteVideo(){
+    if (video.muted){
+        video.muted=false
+        mute.innerHTML='mute'
+    }
+    else {
+        video.muted=true
+        mute.innerHTML='unmute'
+    }
+}
+function volVideo() {
+    volumeDisp.innerHTML = vol.value+'%'
+    video.volume = vol.value/100
+}
